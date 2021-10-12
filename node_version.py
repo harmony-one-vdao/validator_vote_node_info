4eb1b19967f66e069e33a5a3d09f766dfbafde42
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-10-12 10:25:55 +0200

    [UPD] namedtuples for access + check wallet display

--- node_version.py
+++ node_version.py
@@ -2,6 +2,8 @@ from core.blskeys import *
 
 latest_node_version = "v7174-v4.3.0-0-g15f9b2d1"
 
+# check a single wallets vote
+check_wallet = 'one199wuwepjjefwyyx8tc3g74mljmnnfulnzf7a6a'
 
 def get_all_validator_info(
     fn: str, num_pages: int = 10, save_json_data: bool = False
@@ -36,70 +38,67 @@ def get_all_validator_info(
             "params": [i],
             "id": 1,
         }
-        data = post(harmony_api, json=d).json()
+        data = post(harmony_api, json=d).json()["result"]
 
-        if not data["result"]:
+        if not data:
             print(f"NO MORE DATA.. ENDING ON PAGE {i+1}.")
             break
 
-        result += data["result"]
+        result += data
 
-        for x in data["result"]:
-
+        for d in data:
+            show = False
             include = False
             is_elected = False
 
-            v = x["validator"]
+            v, e =create_named_tuple_from_dict(d)
+            if v.address == check_wallet:
+                show = True
 
-            epos_status = x["epos-status"]
-            active_status = x["active-status"]
-
-            keys = v["bls-public-keys"]
-            name = v["name"]
-            address = v["address"]
-            contact = v["security-contact"]
-            website = v["website"]
 
             validators = {x[0]: [] for x in csv_data}
             if not validators.get("name"):
-                validators.update({name: []})
+                validators.update({v.name: []})
                 include = True
 
-            if active_status == "active":
+            if e.active_status == "active":
                 w = []
                 active_validators += 1
 
-                if epos_status == "currently elected":
+                if e.epos_status == "currently elected":
                     elected += 1
                     is_elected = True
 
-                for blskey in keys:
+                for blskey in v.bls_public_keys:
                     found, msg, versions, shard = bls_key_version(
                         blskey, prometheus_data
                     )
 
                     # We only need to register 1 key per shard because it is the binary version not the key that require updating.
-                    if shard not in validators[name]:
-                        validators[name] += [shard]
-                        shards = validators[name]
+                    if shard not in validators[v.name]:
+                        validators[v.name] += [shard]
+                        shards = validators[v.name]
 
                         if (
                             include
                             and (latest_node_version not in versions)
-                            and (not address in updated_but_vers_not_found)
+                            and (not v.address in updated_but_vers_not_found)
                         ):
                             include = False
                             not_updated += 1
+                            if show:
+                                display_check = f'\n\tWallet *- {check_wallet} -* Node Updated = NO!\n'
                             if is_elected:
                                 elected_not_updated += 1
 
+
                             w = [
-                                name,
-                                address,
-                                contact,
-                                website,
-                                epos_status,
-                                active_status,
+                                v.name,
+                                v.address,
+                                v.security_contact,
+                                v.website,
+                                e.epos_status,
+                                e.active_status,
                                 # blskey,
                                 versions,
                             ]
@@ -110,16 +109,20 @@ def get_all_validator_info(
                                 is_updated += 1
                                 if is_elected:
                                     elected_is_updated += 1
+                                if show:
+                                    display_check = f'\n\tWallet *- {check_wallet} -* Node Updated = YES!\n'
+
+
                 if w:
-                    hPoolId = find_smartstakeid(address, smartstake_validator_list)
-                    grouped, app = sort_group(contact)
+                    hPoolId = find_smartstakeid(v.address, smartstake_validator_list)
+                    grouped, app = sort_group(v.security_contact)
                     if app == "unknown":
                         # some validators put twitter in the website column
                         # if unknown, we can try the website column,
-                        # if website == unknown, we will take the unknown from the original contact..
-                        grouped, app = sort_group(website)
+                        # if website == unknown, we will take the unknown from the original v.security_contact..
+                        grouped, app = sort_group(v.website)
                         if app == "unknown":
-                            grouped = [contact]
+                            grouped = [v.security_contact]
 
                     grouped_data[app] += grouped
 
@@ -159,7 +162,7 @@ def get_all_validator_info(
         with open(all_validators_fn, "w") as j:
             dump(result, j, indent=4)
 
-    display_blskey_stats(active_validators, is_updated, not_updated, elected, elected_is_updated, elected_not_updated)
+    display_blskey_stats(active_validators, is_updated, not_updated, elected, elected_is_updated, elected_not_updated, display_check)
 
 
 if __name__ == "__main__":
