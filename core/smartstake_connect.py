68a73414769768d9e9734ab190ab5be8b8f422dd
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-10-13 19:09:16 +0200

    [UPD] Error handling for no SS token

--- core/smartstake_connect.py
+++ core/smartstake_connect.py
@@ -1,7 +1,6 @@
 import os, sys
 
 sys.path.append(os.path.dirname("."))
-
 from core.util import *
 
 
@@ -19,18 +18,26 @@ def smartstake_base(
     ss_data = ss_get.json()
 
     if ss_data.get("errors"):
-        return False, ss_data
+        return False, ss_data.get("errors")
     return True, ss_data
 
 
 def find_smartstakeid(one_address, d: dict) -> int:
-    for x in d["data"]:
-        if x["address"] == one_address:
-            return int(x["hPoolId"])
+    try:
+        for x in d["data"]:
+            if x["address"] == one_address:
+                hPoolId = int(x["hPoolId"])
+                return (
+                    smartstake_address_summary.format(hPoolId),
+                    smartstake_address_blskeys.format(hPoolId),
+                )
+    except TypeError:
+        # log.info("Smartstake Token Not Valid.  Please obtain a new one")
+        return "N/A", "N/A"
 
 
 if __name__ == "__main__":
-    token = "1634037654"
+    token = "1634037"
     res, msg = smartstake_base(token)
 
-    log.info(res, msg)
+    log.info(f"{res}  {msg}")
