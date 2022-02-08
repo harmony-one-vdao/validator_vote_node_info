e5f8efba9bb77e832226c67f6bab49a6cceb6d01
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-02-08 16:12:15 +0100

    [UPD] retry on failure for node version

--- node_version.py
+++ node_version.py
@@ -1,5 +1,7 @@
 import logging
+from time import sleep
 from core.blskeys import *
+
 
 latest_node_version = "v7331-v4.3.4-0-g4ea9072e"
 #  v7331-v4.3.4-0-g4ea9072e-dirty
@@ -144,16 +146,27 @@ def validator_node_version(
         display_blskey_stats,
         save_json_data=save_json_data,
     )
+    return True
 
 
 if __name__ == "__main__":
     latest_version = latest_node_version.split("-")[1]
     create_folders_change_handler(latest_version)
-    validator_node_version(
-        node_version_fn,
-        latest_version,
-        grouped_data,
-        num_pages=100,
-        save_json_data=True,
-        check_wallet=check_wallet,
-    )
+    
+    c = 1
+    while 1:
+        log.info(f'{c}) Attempting to get data...')
+        res = validator_node_version(
+            node_version_fn,
+            latest_version,
+            grouped_data,
+            num_pages=100,
+            save_json_data=True,
+            check_wallet=check_wallet,
+        )
+        if res:
+            break
+        else:
+            sleep(10)
+            log.error('Unable to connect.  Retrying.. ')
+        c+=1
